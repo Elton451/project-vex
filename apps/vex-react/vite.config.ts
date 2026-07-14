f1d47aebc7f2eeab1faddf643d5cebabb2ca8cb5
Elton <eltonls@proton.me>
2026-07-13 21:12:22 -0300

    chore: add setup for tests with RTL and Vitest

--- apps/vex-react/vite.config.ts
+++ apps/vex-react/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
@@ -5,11 +6,17 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-	tanstackRouter({
-		target: "react",
-		autoCodeSplitting: true,
-	}),
+	test: {
+		globals: true,
+		environment: "jsdom",
+		setupFiles: "./src/tests/setup.ts",
+		include: ["**/*.{test,spec}.{js,ts,tsx,jsx}"],
+	},
   plugins: [
+		tanstackRouter({
+			target: "react",
+			autoCodeSplitting: true,
+		}),
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
